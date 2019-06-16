d66fe314e6908247d4ca24a365a8382901954d93
Hcl20171107 <43674629+Hcl20171107@users.noreply.github.com>
2019-06-16 21:32:57 +0800

    Create Grade.java

---
+++ HAHA/src/haha/Grade.java
@@ -0,0 +1,43 @@
+package haha;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Grade {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Grade window = new Grade();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Grade() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
